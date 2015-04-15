20cb54ff9d12461a5326d2e61b050ae2ddd15edc
Vincent Laurier <vincent.laurier@laposte.net>
2015-04-15 14:19:48 +0200

    remove comments in Game

--- src/Patterns/Observer/Game.php
+++ src/Patterns/Observer/Game.php
@@ -8,35 +8,30 @@ class Game implements \SplSubject
     private $number;
     private $observers = array();
 
-    //add observer
     public function attach(\SplObserver $observer)
     {
         $this->observers[] = $observer;
     }
 
-    //remove observer
     public function detach(\SplObserver $observer)
     {
-
         $key = array_search($observer, $this->observers, true);
         if (false !== $key) {
             unset($this->observers[$key]);
         }
     }
 
-    //turn
-    public function turn()
-    {
-        $this->number = rand(1, 49);
-        $this->notify();
-    }
-
-    //notify observers
     public function notify()
     {
         foreach ($this->observers as $observer) {
             $observer->update($this);
         }
+    }
+
+    public function turn()
+    {
+        $this->number = rand(1, 49);
+        $this->notify();
     }
 
     public function getNumber()
