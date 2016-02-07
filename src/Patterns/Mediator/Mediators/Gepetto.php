e0cd2f71622c4ec59ee69d8d121f7dd087fdcb32
Laurier Vincent <vincent.laurier@laposte.net>
2016-02-07 09:54:36 +0100

    WIP

--- src/Patterns/Mediator/Mediators/Gepetto.php
+++ src/Patterns/Mediator/Mediators/Gepetto.php
@@ -10,33 +10,33 @@ class Gepetto implements Mediator
     public function respondTo(Colleague $sender):bool
     {
         if (!$sender->isReady()) {
-            echo 'Gepetto (à '.$sender.'): Non, tu ne peux pas ouvrir. Prépare toi !';
+            echo "Gepetto (à ".$sender."): Prépare toi d'abord s'il te plaît !";
             return false;
         }
         if ($sender instanceof Torp) {
-            echo 'Gepetto (à '.$sender.'): Patiente, je vérifie que tout est bon à la billetterie.<br/>';
+            echo 'Gepetto (à Torp): Je vérifie à la billetterie.<br/>';
             echo '<em>Gepetto contacte Pearl</em><br/>';
             return $this->checkTicketCounter();
         }
         if ($sender instanceof Pearl) {
-            echo 'Gepetto (à Pearl): Patiente, je vérifie que tout est prêt au chapiteau.<br/>';
+            echo 'Gepetto (à Pearl): Je vérifie du côté du chapiteau.<br/>';
             echo '<em>Gepetto contacte Solid</em><br/>';
             return $this->checkCircusTent();
         }
         if ($sender instanceof Solid) {
-            echo "Gepetto (à Solid): C'est parfait tu es prêt, on va pouvoir ouvrir le chapiteau.<br/>";
+            echo "Gepetto (à Solid): Tu es prêt, donc le chapiteau aussi !<br/>";
             return true;
         }
 
-        throw new \Exception("Gepetto n'est pas en mesure de gérer cette situation");
+        throw new \Exception("Situation non gérée par Gepetto");
     }
 
     private function checkTicketCounter()
     {
         $response = (new Pearl($this))->canIOpen();
         ($response)
-            ? print "Torp, La billetterie est prête, tu peux ouvrir le parc.<br/>"
-            : print "La billeterie n'est pas prête, attend mon feu vert Torp !<br/>";
+            ? print "Torp, la billetterie est prête. Ouvre le parc !<br/>"
+            : print "Torp attend ! La billeterie n'est pas prête !<br/>";
         return $response;
     }
 
@@ -44,8 +44,8 @@ class Gepetto implements Mediator
     {
         $response = (new Solid($this))->canIOpen();
         ($response)
-            ? print "Pearl, le chapiteau est prêt, tu peux ouvrir la billetterie<br/>"
-            : print "La chapiteau n'est pas prêt, attend mon feu vert Pearl !<br/>";
+            ? print "Pearl, le chapiteau est prêt. Ouvre la billetterie !<br/>"
+            : print "Pearl attend ! Le chapiteau n'est pas prêt !<br/>";
         return $response;
     }
 }
--- src/Patterns/Mediator/demo.php
+++ src/Patterns/Mediator/demo.php
@@ -1,13 +1,11 @@
 <?php
 use Solid\Patterns\Mediator\Mediators\Gepetto;
-use Solid\Patterns\Mediator\Colleagues\{Torp, Pearl, Solid};
+use Solid\Patterns\Mediator\Colleagues\Torp;
 
 require "../../../vendor/autoload.php";
 header('Content-Type: text/html; charset=utf-8');
 
 $gepetto = new Gepetto();
 $torp = new Torp($gepetto);
-$pearl = new Pearl($gepetto);
-$solid = new Solid($gepetto);
 
 $torp->canIOpen();
